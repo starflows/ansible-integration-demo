79ab4e8ca585eecad2c59d2a2c77a35fd34e7f4f
Stefan Mückstein <stefan@cloudomation.com>
2019-05-21 13:13:53 +0200

    target setting, copy files

--- ansible-test.py
+++ ansible-test.py
@@ -1,8 +1,10 @@
 def handler(system, this):
     this.flow(
-        'ansible.play',
+        'ansible-play',
         playbook='verify-apache.yaml',
+        target='ansible-host-1',
         become=True,
+        files=['test-site.conf.j2', 'index.html.j2'],
         run=True,
     )
     return this.success('all done')
